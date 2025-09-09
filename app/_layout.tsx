ca13e10f62364b4912ff96cab6c7658131e4aeb8
Alexander <105300503+AlexWayke@users.noreply.github.com>
2025-09-09 15:05:53 +0400

    Feat: added  mobile extentions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,18 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   const isAuth = false;
-console.log('check')
+  
   return (
-    <Stack>
-      <AuthProvider>
+    <AuthProvider>
+      <Stack>
         <Stack.Protected guard={isAuth}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)"/>
         </Stack.Protected>
 
         <Stack.Protected guard={!isAuth}>
           <Stack.Screen name="auth" />
         </Stack.Protected>
-      </AuthProvider>
-    </Stack>
+      </Stack>
+    </AuthProvider>
   );
 }
