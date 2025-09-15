285a0b4998eed726f5008c26724fa49ebc481e23
Alexander <105300503+AlexWayke@users.noreply.github.com>
2025-09-15 18:10:10 +0400

    Feat: added add habit page content

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,59 @@
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/lib/appwrite";
 import { useAuth } from "@/lib/auth-context";
-import { Text, View, StyleSheet } from "react-native";
-import { Button } from "react-native-paper";
+import { Habit } from "@/types/database.type";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Query } from "appwrite";
+import { useCallback, useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Button } from "react-native-paper";
 
 export default function Index() {
-const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const [habits, setHabits] = useState<Habit[]>();
+
+  const fetchHabits = useCallback(async () => {
+    try{
+      const response = await databases.listDocuments(
+        DATABASE_ID!, 
+        HABITS_COLLECTION_ID!,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+
+      console.log(response)
+      setHabits(response.documents as Habit[]);
+    } catch (error) {
+      console.error(error)
+    }
+  }, [user?.$id])
+
+  useEffect(() => {
+    fetchHabits();
+  }, [fetchHabits, user])
 
   return (
     <View style={styles.view}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button mode="text" onPress={signOut} icon={"logout"}>Sign Out</Button>
+      <View>
+        <Text variant="headlineSmall">Today`s habits</Text>
+        <Button mode="text" onPress={signOut} icon={"logout"}>Sign Out</Button>
+      </View>
+
+      {habits?.length === 0 ? (
+        <View>
+          <Text>No habits yet. Add your first habit.</Text>
+        </View>
+      ) : (
+        habits?.map((habit, key) => (
+          <View key={key}>
+            <Text>{habit.title}</Text>
+            <Text>{habit.description}</Text>
+            <View>
+              <View>
+                <MaterialCommunityIcons name="fire" size={18} color={"#ff9800"} />
+              </View>
+            </View>
+          </View>
+        ))
+      )}
     </View>
   );
 }
