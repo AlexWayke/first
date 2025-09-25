dcdc73ee44d7f23d2805ba6ee79d215ba0a8add3
Alexander <105300503+AlexWayke@users.noreply.github.com>
2025-09-25 21:38:18 +0400

    Feat: added habits list

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 function RouteProtecter ({children}: {children: React.ReactNode}) {
   const router = useRouter();
@@ -27,14 +28,16 @@ function RouteProtecter ({children}: {children: React.ReactNode}) {
 export default function RootLayout() {
   
   return (
-    <AuthProvider>
-      <PaperProvider>
-        <SafeAreaProvider>
-          <RouteProtecter>
-            <Stack.Screen name="(tabs)" options={{headerShown: false}} />
-          </RouteProtecter>
-        </SafeAreaProvider>
-      </PaperProvider>
-    </AuthProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <AuthProvider>
+        <PaperProvider>
+          <SafeAreaProvider>
+            <RouteProtecter>
+              <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+            </RouteProtecter>
+          </SafeAreaProvider>
+        </PaperProvider>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
